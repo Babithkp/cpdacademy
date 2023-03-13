5ab503cc6116cb6fafe936f095a846f734214446
Babithkp <babithkullachetti44@gmail.com>
2023-03-13 15:15:25 +0500

    Initial Commit Just added copied homepage and nothing changed in it

---
+++ project/routes.py
@@ -0,0 +1,12 @@
+from project import app
+from flask import render_template
+
+
+@app.route("/")
+def home():
+	return render_template("home.html")
+
+
+@app.route("/rule")
+def rule():
+	pass
---
+++ web.py
@@ -0,0 +1,5 @@
+#/usr/bin/python3
+
+from project import app
+
+app.run(host="", port=8080, debug=True)
