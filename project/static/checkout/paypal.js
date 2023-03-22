05542b964f476114b643cd537938918428d42d19
Babithkp <babithkullachetti44@gmail.com>
2023-03-22 19:04:12 +0500

    Edited homepage HealthCare img and added custom text Cropped text from image

--- project/static/checkout/paypal.js
+++ project/static/checkout/paypal.js
@@ -1,46 +1,46 @@
-    function initPayPalButton() {
-      paypal.Buttons({
-        style: {
-          shape: 'rect',
-          color: 'blue',
-          layout: 'vertical',
-          label: 'paypal',
-          
-        },
+function initPayPalButton() {
+  paypal.Buttons({
+    style: {
+      shape: 'rect',
+      color: 'blue',
+      layout: 'vertical',
+      label: 'paypal',
 
-        onInit: function(data, actions) {
-          // actions.disable();
-        },
-        createOrder: function(data, actions) {
-          return actions.order.create({
-            purchase_units: [{"description":"Care Asssistant Course ","amount":{"currency_code":"GBP","value":150}}]
-          });
-        },
+    },
 
-        onApprove: function(data, actions) {
-          return actions.order.capture().then(function(orderData) {
-            
-            // Full available details
-            console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+    onInit: function (data, actions) {
+      // actions.disable();
+    },
+    createOrder: function (data, actions) {
+      return actions.order.create({
+        purchase_units: [{ "description": "Care Asssistant Course ", "amount": { "currency_code": "GBP", "value": 150 } }]
+      });
+    },
 
-            // Show a success message within this page, e.g.
-            const element = document.getElementById('paypal-button-container');
-            element.innerHTML = '';
-            element.innerHTML = '<h3>Thank you for your payment!</h3>';
+    onApprove: function (data, actions) {
+      return actions.order.capture().then(function (orderData) {
 
-            // Or go to another URL:  actions.redirect('thank_you.html');
-            
-          });
-        },
+        // Full available details
+        console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
 
-        onError: function(err) {
-          console.log(err);
-        }
-      }).render('#paypal-button-container');
+        // Show a success message within this page, e.g.
+        const element = document.getElementById('paypal-button-container');
+        element.innerHTML = '';
+        element.innerHTML = '<h3>Thank you for your payment!</h3>';
+
+        // Or go to another URL:  actions.redirect('thank_you.html');
+
+      });
+    },
+
+    onError: function (err) {
+      console.log(err);
     }
+  }).render('#paypal-button-container');
+}
 
-    function validateForm() {
+function validateForm() {
 
-    }
+}
 
-    initPayPalButton();
+initPayPalButton();
