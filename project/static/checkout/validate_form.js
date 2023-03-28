90598bacb9fc839f0419280831225e6e6317b3d0
Babithkp <babithkullachetti44@gmail.com>
2023-03-28 12:40:00 +0500

    Validating Signup Form Using JavaScript

---
+++ project/static/checkout/validate_form.js
@@ -0,0 +1,17 @@
+var f_name = document.getElementById("billing_first_name")
+var l_name = document.getElementById("billing_last_name")
+var company = document.getElementById("billing_company")
+var country = document.getElementById("billing_country")
+var addr_1 = document.getElementById("billing_address_1")
+var addr_2 = document.getElementById("billing_address_2")
+var city = document.getElementById("billing_city")
+var postal_code = document.getElementById("billing_postcode")
+var phone = document.getElementById("billing_phone")
+var email = document.getElementById("billing_email")
+var passwd = document.getElementById("account_password")
+var c_passwd = document.getElementById("account_confirm_password")
+
+
+function validate_form() {
+    return false
+}
