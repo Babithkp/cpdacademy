402caf7ea35b4939d549b621de25cca3a197c8b5
Babithkp <babithkullachetti44@gmail.com>
2023-04-02 17:58:18 +0500

    Separated overlayJS from validate_form.js files

--- project/static/checkout/validate_form.js
+++ project/static/checkout/validate_form.js
@@ -94,14 +94,3 @@ async function send_data(form, url) {
             })
     return output;
 }
-
-
-var overlay = document.getElementById("custom-overlay");
-
-function show_overlay() {
-    overlay.style.top = "0%"
-}
-function hide_overlay() {
-    overlay.style.top = "100%"
-}
-// show_overlay()
---
+++ project/static/overlay.js
@@ -0,0 +1,9 @@
+var overlay = document.getElementById("custom-overlay");
+
+function show_overlay() {
+    overlay.style.top = "0%"
+}
+function hide_overlay() {
+    overlay.style.top = "100%"
+}
+// show_overlay()
